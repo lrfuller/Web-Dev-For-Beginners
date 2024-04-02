225c645c3599cd631d35f11233c28a73d7e55af5
LukasFuller <33261077+lrfuller@users.noreply.github.com>
2024-04-02 17:25:15 -0500

    form initial registration working

---
+++ 7-bank-project/_Self_version/app.js
@@ -0,0 +1,61 @@
+const routes = {
+  "/login": { templateId: "login" },
+  "/dashboard": { templateId: "dashboard" },
+  "/profile": { templateId: "profile" },
+};
+
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  navigate(event.target.href);
+}
+
+function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
+
+  if (!route) {
+    return navigate("/login");
+  }
+
+  const template = document.getElementById(route.templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+  app.appendChild(view);
+  // document.getElementById("appTitle").innerHTML = route.templateId;
+  document.title = route.templateId;
+}
+updateRoute("login");
+window.onpopstate = () => updateRoute();
+
+async function createAccount(account) {
+  try {
+    const response = await fetch("//localhost:5000/api/accounts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: account,
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || "Unknown error" };
+  }
+}
+
+async function register() {
+  const registerForm = document.getElementById("registerForm");
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    return console.log("An error occurred:", result.error);
+  }
+
+  console.log("Account created!", result);
+}
